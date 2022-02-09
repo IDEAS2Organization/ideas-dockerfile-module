c559b48306cb829224aeeb29007fe4219cf63ca3
Francisco-Borrego <fraborcar@alum.us.es>
2022-02-09 18:13:04 +0100

    Commit inicial

---
+++ src/main/java/es/us/isa/ideas/controller/dockerfile/Dockerfile4IdeasModuleApplication.java
@@ -0,0 +1,11 @@
+package es.us.isa.ideas.controller.dockerfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dockerfile4IdeasModuleApplication {
+        public static void main(String[] args){
+            SpringApplication.run(Dockerfile4IdeasModuleApplication.class,args);
+        }
+}
---
+++ src/main/java/es/us/isa/ideas/controller/dockerfile/DockerfileLanguageController.java
@@ -0,0 +1,60 @@
+package es.us.isa.ideas.controller.dockerfile;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import es.us.isa.ideas.module.common.AppResponse;
+import es.us.isa.ideas.module.common.AppResponse.Status;
+import es.us.isa.ideas.module.controller.BaseLanguageController;
+import javax.servlet.http.HttpServletRequest;
+
+
+@Controller
+@RequestMapping("/ideas-plaintext-language/language")
+public class DockerfileLanguageController extends BaseLanguageController {
+
+	@RequestMapping(value = "/format/{format}/checkLanguage", method = RequestMethod.POST)
+	@ResponseBody
+        @Override
+	public AppResponse checkLanguage(String id, String content, String fileUri,HttpServletRequest request) {
+		
+		AppResponse appResponse = new AppResponse();
+		
+		boolean problems = false;
+
+		//System.out.println("CheckSyntax: " + res );
+		appResponse.setFileUri(fileUri);
+		
+		if (problems)
+			appResponse.setStatus(Status.OK_PROBLEMS);
+		else
+			appResponse.setStatus(Status.OK);
+		
+		
+		return appResponse;
+	}
+
+	@RequestMapping(value = "/convert", method = RequestMethod.POST)
+	@ResponseBody
+        @Override
+	public AppResponse convertFormat(String currentFormat, String desiredFormat, String fileUri, String content,HttpServletRequest request) {
+		AppResponse appResponse = new AppResponse();
+		
+		return appResponse;
+	}
+
+	@RequestMapping(value = "/operation/{id}/execute", method = RequestMethod.POST)
+	@ResponseBody
+        @Override
+	public AppResponse executeOperation(String id, String content, String fileUri, String auxArg0,HttpServletRequest request) {
+		AppResponse appResponse = new AppResponse();
+		return appResponse;
+	}
+	
+	// Analysis operations:
+	
+	
+
+}
